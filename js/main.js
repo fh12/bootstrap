3f19995b30f9063a7b68e668ccf85399003fbab3
fh12 <18505565993@163.com>
2019-09-11 23:13:11 +0800

    更新头部和底部

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  var windows = $(window);
+    var stick = $(".header-sticky");
+	windows.on('scroll',function() {    
+		var scroll = windows.scrollTop();
+		if (scroll < 245) {
+			stick.removeClass("sticky");
+		}else{
+			stick.addClass("sticky");
+		}
+	}); 
+})
