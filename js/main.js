979f43de1e1fb2df7ca56bda63343ab50b09c77c
fh12 <18505565993@163.com>
2019-09-14 00:29:41 +0800

    主页制作完成

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $(function(){
-  var windows = $(window);
-    var stick = $(".header-sticky");
+	var windows = $(window);
+	// 头部滚动时贴附浏览器顶端
+  var stick = $(".nav-bar");
 	windows.on('scroll',function() {    
 		var scroll = windows.scrollTop();
-		if (scroll < 245) {
+		if (scroll < 105) {
 			stick.removeClass("sticky");
 		}else{
 			stick.addClass("sticky");
 		}
-	}); 
+	});
+	$('.m-nav').click(function(){
+		$('.menu').toggleClass('active')
+		$('.m-nav span').toggleClass('active')
+	})
 })
